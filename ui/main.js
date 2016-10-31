4bffc59b013f5d6b2708c11e1274d34291dd857e
ArvindGanesan <imadmooc@gmail.com>
2016-10-31 09:45:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,27 @@
-console.log('Loaded!');
+//Counter code
+var button = getElementById(`counter`);
+button.onclick = function() {
+    
+   //Create request
+     var request = new XMLHttpRequest();
+     
+   //request function
+     request.onreadystatechange = functon () {
+         if(request.readystate === XMLHttpRequest.DONE)
+           {
+               //Take  an action
+             if(request.readystatus === 200 )
+               {
+                var counter = request.responsetext;
+                var span = document.getElementById(`count`);
+                span.innerHTML = counter.toString();
+           }
+          //no action
+         
+     };
+   //make request
+    request.open(`GET`,)
+    
+    
+};
+
