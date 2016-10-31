ef235b7a7e0259e1b4245dd78720957e89ad45d6
ArvindGanesan <imadmooc@gmail.com>
2016-10-31 10:10:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,30 @@
 //Counter code
 var button = document.getElementById(`counter`);
+var counter = 0;
 button.onclick = function() {
     
    //Create request
-     var request = new XMLHttpRequest();
+     //var request = new XMLHttpRequest();
      
    //request function
-     request.onreadystatechange = function () {
-         if(request.readystate === XMLHttpRequest.DONE)
-           {
+     //request.onreadystatechange = function () {
+       //  if(request.readystate === XMLHttpRequest.DONE)
+         //  {
                //Take  an action
-             if(request.status === 200 )
-               {
-                var counter = 4//request.responseText;
+             //if(request.status === 200 )
+              // {
+                //var counter = request.responseText;
+                counter = counter + 1;
                 var span = document.getElementById(`count`);
                 span.innerHTML = counter.toString();
-           }
-           }
+           //}
+          // }
           //no action
          
-     };
+//     };
    //make request
-    request.open('GET','http://arvindganesan.imad.hasura-app.io/counter' , true);
-    request.send(null);
+  //  request.open('GET','http://arvindganesan.imad.hasura-app.io/counter' , true);
+    //request.send(null);
     
     
 };
